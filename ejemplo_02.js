91da96dfea400cb03aeac5839f0697995760b8d9
osmak <osmak@protonmail.com>
2018-10-23 19:01:35 +0200

    modified:   ejemplo_02.js

--- ejemplo_02.js
+++ ejemplo_02.js
@@ -4,12 +4,14 @@ var numpeticiones=0;
 /* Creación del servidor HTTP, y se define la escucha
 de las peticiones en el puerto 300 */
 http.createServer(function(request,response) { 
-    
+    console.log(request.rawHeaders);
     //Se define la cabecera HTTP, con el estado HTTP {OK: 200} y el tipo
     response.writeHead(200, {'Content-Type':'text/html'});
     numpeticiones++;
     //Se responde, en el cuerpo de la resupuesta con el mensaje "Hello World"
-    response.end('<h1>Hola Mundo!</h1><hr><b>Numero de peticiones:</b> '+numpeticiones);
+    response.write('<h1>Hola Mundo!</h1><hr>');
+    response.write('<b>Numero de peticiones:</b> '+numpeticiones)
+    response.end();
 }).listen(3000);
 
 //Se escribe la URL para el acceso al servidor
