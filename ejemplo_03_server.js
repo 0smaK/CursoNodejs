77f09e88fafd1ddbdc7af715fc2e1f7272a96cd6
osmak <osmak@protonmail.com>
2018-10-23 19:38:11 +0200

    modified:   ejemplo_03_server.js

--- ejemplo_03_server.js
+++ ejemplo_03_server.js
@@ -4,16 +4,17 @@ var url = require("url");
 function iniciar() {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        console.log("Peticion para " + pathname + " recibida.");
-        response.writeHead(200, {"Content-Type":"text/html"});
+        console.log("Petición para " + pathname + " recibida.");
+        response.writeHead(200, {"Content-Type": "text/html"});
         response.write("<!DOCTYPE html>");
         response.write("<html>");
         response.write("<head><meta charset='UTF-8'><title>Ejemplo 3 [server]</title>");
-        response.write("<body><center><h2>Hola Mundo</h2><hr></center></body>");
-        response.write("</html>")
+        response.write("<body><center><h2>Servidor con Node.js</h2><hr></center></body>");
+        response.write("Ejemplo 3")
+        response.end("</html>");
     }
 
-    http.createServer(onRequest).listen(300);
+    http.createServer(onRequest).listen(3000);
     console.log("Servidor iniciado.");
 }
 
