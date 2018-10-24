8fb7c91198a0e51cc766ea14f835ffc530a60be2
osmak <osmak@protonmail.com>
2018-10-24 16:45:44 +0200

    added files

--- ejemplo_05/ejemplo_05_handlers.js
+++ ejemplo_05/ejemplo_05_handlers.js
@@ -1,12 +1,12 @@
 var numPeticionesSubir = 0;
 
 function iniciar() {
-  console.log("Manipulador de peticiÃ³n 'iniciar' ha sido llamado.");
+  console.log("Manipulador de petición 'iniciar' ha sido llamado.");
 }
 
 function subir() {
   numPeticionesSubir++;
-  console.log("Manipulador de peticiÃ³n 'subir' ha sido llamado "+numPeticionesSubir+" veces.");
+  console.log("Manipulador de petición 'subir' ha sido llamado "+numPeticionesSubir+" veces.");
 }
 
 exports.iniciar = iniciar;
