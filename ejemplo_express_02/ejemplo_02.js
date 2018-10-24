6a0f2fcb497e0c84f6eb276da2cad85861cae31f
osmak <osmak@protonmail.com>
2018-10-24 20:05:10 +0200

    deleted commented code, desproposito tenerlo ahi

--- ejemplo_express_02/ejemplo_02.js
+++ ejemplo_express_02/ejemplo_02.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var app = express();
 
-/*
-app.get('/', function(req, res) {
-    res.send("Hola GETTT!!!");
-});
-
-app.post('/', function(req, res) {
-    res.send("Hola POSTTT!!!");
-});
-*/
-
 app.all('/', function(req, res) {
     res.send("Hola Mundo!!!<br>Escuchando todos los puertos!!!");
 });
